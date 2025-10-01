293101e4b0bd75fe4cad3a1b2856de87b893fb23
copilot0805 <167594105+copilot0805@users.noreply.github.com>
2025-10-01 17:31:23 +0700

    .

--- lab2/Core/Inc/main.h
+++ lab2/Core/Inc/main.h
@@ -57,6 +57,8 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define DOT_Pin GPIO_PIN_4
+#define DOT_GPIO_Port GPIOA
 #define LED_RED_Pin GPIO_PIN_5
 #define LED_RED_GPIO_Port GPIOA
 #define EN0_Pin GPIO_PIN_6
@@ -69,6 +71,10 @@ void Error_Handler(void);
 #define LED_7SEG_B_GPIO_Port GPIOB
 #define LED_7SEG_C_Pin GPIO_PIN_2
 #define LED_7SEG_C_GPIO_Port GPIOB
+#define EN2_Pin GPIO_PIN_8
+#define EN2_GPIO_Port GPIOA
+#define EN3_Pin GPIO_PIN_9
+#define EN3_GPIO_Port GPIOA
 #define LED_7SEG_D_Pin GPIO_PIN_3
 #define LED_7SEG_D_GPIO_Port GPIOB
 #define LED_7SEG_E_Pin GPIO_PIN_4
